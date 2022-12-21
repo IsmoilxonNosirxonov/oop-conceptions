6a0c3701cc1bc03295cf5baf98d6e73d1f0328d5
Omadjon Boymurodov <OmadjonBoymurodov@gmail.com>
2022-12-21 18:34:52 +0500

    hdfsds

--- src/service/CardService.java
+++ src/service/CardService.java
@@ -5,10 +5,19 @@ import model.Card;
 import java.util.UUID;
 
 public class CardService extends BaseService{
-
+private int index=0;
     @Override
     protected boolean add(Object object) {
-        return false;
+        Card card=(Card) object;
+        for (Card card1:cardList) {
+            if(card1!=null){
+                if(card1.getCardNumber().equals(card.getCardNumber())){
+                    return false;
+                }
+            }
+        }
+        cardList[index++]=card;
+        return true;
     }
 
     @Override
--- src/service/UserService.java
+++ src/service/UserService.java
@@ -8,14 +8,30 @@ import model.HistoryType;
 import java.util.UUID;
 
 public class UserService extends BaseService{
-
+private int index=0;
     @Override
     protected boolean add(Object object) {
-        return false;
+        User user = (User) object;
+        for (User user1:userList) {
+            if(user1!=null){
+                if(user1.getFullName().equals(user.getFullName())){
+                    return false;
+                }
+            }
+        }
+        userList[index++]= user;
+        return true;
     }
 
     @Override
     protected Object getById(UUID id) {
+        for (User user : userList) {
+            if(user!=null){
+                if(user.getId().equals(id)){
+                    return user;
+                }
+            }
+        }
         return null;
     }
 }
