52a35703e9d7fff60fe6b76b6314be3eebb75d55
Omadjon Boymurodov <OmadjonBoymurodov@gmail.com>
2022-12-21 18:04:04 +0500

    commit  changes

--- src/service/AuthService.java
+++ src/service/AuthService.java
@@ -1,13 +1,21 @@
 package service;
 
+import model.User;
+
 public class AuthService {
+      private static User userList[]=new User[100];
 
     public boolean login(
             final String username,
             final String password
     ){
-
-
-        return true;
+        for (User user:userList) {
+            if(user!=null){
+                if(user.getFullName().equals(user.getFullName()) && user.getPassword().equals(user.getPhoneNumber())){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,11 +1,16 @@
 package service;
 
+import model.User;
+
 import java.util.UUID;
 
 public class UserService extends BaseService{
 
     @Override
     protected boolean add(Object object) {
+        User user=(User) object;
+
+
         return false;
     }
 
