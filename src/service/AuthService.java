0dd957aad7994ab7a9548c915efe885d31f1e57c
Davronjon Rasulov <davronjonr3@gmail.com>
2022-12-21 18:03:15 +0500

    some chenjes

--- src/service/AuthService.java
+++ src/service/AuthService.java
@@ -1,13 +1,22 @@
 package service;
 
+import model.User;
+
 public class AuthService {
+    public User[] userList=new User[1000];
 
     public boolean login(
             final String username,
             final String password
     ){
+        for (User user:userList) {
+            if(user!=null){
+                if(user.getFullName().equals(username) && user.getPassword().equals(password)){
+                    return true;
+                }
+            }
+        }
+        return  false;
 
-
-        return true;
     }
 }
