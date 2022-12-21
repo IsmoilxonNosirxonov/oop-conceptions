0e1ac7aa329b798c4bb40386d57c8448439a1f66
Asadbek Ziyodullayev <ziyodullayevasadbek@gmail.com>
2022-12-21 18:03:01 +0500

    some changes

--- src/service/AuthService.java
+++ src/service/AuthService.java
@@ -1,13 +1,20 @@
 package service;
 
-public class AuthService {
+import model.User;
 
+public class AuthService {
+    public static User[] userList = new User[100];
     public boolean login(
             final String username,
             final String password
     ){
-
-
-        return true;
+        for (User user:userList) {
+            if(user != null){
+                if(user.getFullName().equals(username) && user.getPassword().equals(password)){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,12 +1,23 @@
 package service;
 
+import model.User;
+
 import java.util.UUID;
 
 public class UserService extends BaseService{
-
+    private int index = 0;
     @Override
     protected boolean add(Object object) {
-        return false;
+        User user = (User) object;
+        for (User u:AuthService.userList) {
+            if(u != null){
+                if(u.getFullName().equals(user.getFullName()){
+                    return false;
+                }
+            }
+        }
+        AuthService.userList[index++] = user;
+        return true;
     }
 
     @Override
