c68be58b10bfb9b3709632c99e449bc16bdcea09
Omadjon Boymurodov <OmadjonBoymurodov@gmail.com>
2022-12-22 18:53:46 +0500

    hdfsds

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import service.CardService;
+import service.P2PService;
+import service.UserService;
+
 import java.util.Scanner;
 
 public class Main {
@@ -5,11 +9,25 @@ public class Main {
 
         Scanner scannerStr=new Scanner(System.in);
         Scanner scannerInt=new Scanner(System.in);
+        CardService cardService=new CardService();
+        UserService userService=new UserService();
+        P2PService p2PService=new P2PService();
       int stepcode=2;
 
       while (stepcode!=0){
-          System.out.println("1 Add user 2 Login 0 Exit");
+          System.out.println("1 Add user 2 Login");
+          stepcode= scannerStr.nextInt();
+          switch (stepcode) {
+              case 1->{
+                  System.out.println("Entr username");
+                  String username= scannerStr.nextLine();
+                  System.out.println("Entr username");
+                  String password= scannerInt.nextLine();
+              }
+              case 2->{
 
+              }
+          }
       }
 
     }
