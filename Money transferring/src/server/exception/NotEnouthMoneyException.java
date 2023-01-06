6d7daf4c8d69f69c74be8fc9d25ef98fb0807894
Nobleman2704 <113179121+Nobleman2704@users.noreply.github.com>
2023-01-06 17:40:54 +0500

    Delete Money transferring directory

--- Money transferring/src/server/exception/NotEnouthMoneyException.java
+++
@@ -1,7 +0,0 @@
-package server.exception;
-
-public class NotEnouthMoneyException extends Exception{
-    public NotEnouthMoneyException(String message){
-        super(message);
-    }
-}
--- Money transferring/src/server/model/TransferType.java
+++
@@ -1,5 +0,0 @@
-package server.model;
-
-public enum TransferType {
-    DEBIT, CREDIT
-}
