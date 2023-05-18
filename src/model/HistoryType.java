a325cd9d339b7aa59d52938e10ffdc53d5ecb718
Imomxo'ja <121791597+Imomxoja@users.noreply.github.com>
2023-05-18 17:43:05 +0500

    Delete src directory

--- src/model/HistoryType.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public enum HistoryType {
-    DEBIT, // kirim
-    CREDIT // chiqim
-}
--- src/service/P2PService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-public class P2PService {
-
-    public boolean p2p(final String senderCardNumber, final String receiverCardNumber, final double amount){
-        // istoriyani ham shu yerda qilib ketasizlar
-        return true;
-    }
-}
